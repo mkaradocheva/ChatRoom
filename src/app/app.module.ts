1eab4280f59ad437c55cf740dcbffcc636b21eb8
Mihaela Karadocheva <mkaradocheva6@gmail.com>
2020-02-21 19:48:03 +0200

    Added some validations for registration form, still in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RulesComponent } from './rules/rules.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RulesComponent } from './rules/rules.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  form;
+  emailPattern: string= '/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', [Validators.required,
+         Validators.minLength(3), 
+         Validators.maxLength(15)]
+        ],
+      email: ['', [Validators.required,
+        Validators.pattern(this.emailPattern)]],
+      password: ['', Validators.required],
+    repeatPassword: ['']
+    });
+    
   }
 
+  register(){
+    console.log(this.form);
+  }
+
+  get f(){
+    return this.form.controls;
+  }
 }
